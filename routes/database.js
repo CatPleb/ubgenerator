e707a44a742c956eb8c32e440b3e7e34f458dbb6
CatPleb <48354679+CatPleb@users.noreply.github.com>
2019-04-09 18:05:09 +0200

    Tag deleting now possible

--- routes/database.js
+++ routes/database.js
@@ -44,7 +44,6 @@ router.post('/create_tags', adminsecured(), function(req,res,next) {
 router.post('/engrave_new_tag', adminsecured(), async function(req,res,next) {
 
   var boo = await myhelper.check_and_engrave(req.body.tag_name, req.body.parent_tags, req.body.child_tags);
-  console.log(boo);
 
   if (boo[0] == false) {
     req.session.error = boo[1];
@@ -59,8 +58,40 @@ router.post('/engrave_new_tag', adminsecured(), async function(req,res,next) {
   }
 });
 
+
+// html site for form to delete exisiting tag
 router.post('/delete_tags', adminsecured(), function(req,res,next) {
-  res.render('database/delete_tags', {})
+  Hierarchy.find().then(function(result) {
+    tagList = myhelper.get_tags(result);
+    res.render('database/delete_tags', {tagList: tagList, create_error: [req.session.error] });
+    req.session.error = undefined;
+  }).catch(function(err) {
+    if (err) throw err;
+  });
+});
+
+
+// check for valid input and delete tag if valid
+router.post('/kill_existing_tag', adminsecured(), async function(req,res,next) {
+  if ( req.body.selected_tag.localeCompare(req.body.typed_tag) != 0 ) {
+    req.session.error = 'ERROR: Selected tag has to equal typed tag.';
+    res.redirect(307, '/database/delete_tags');
+    return;
+  }
+
+  var boo = await myhelper.check_and_kill(req.body.typed_tag);
+
+  if (boo[0] == false) {
+    req.session.error = boo[1];
+    res.redirect(307,'/database/delete_tags');
+  } else if (boo[0] == true) {
+    req.session.error = 'Tag deleted succesfully. Database updated.';
+    res.redirect(307, '/database/delete_tags');
+  } else {
+    console.log('ERROR: Something went terribly wrong!\nboo[0] = '+boo[0]+'\nboo[1] = '+boo[1]);
+    req.session.error = 'ERROR: Critical Error!';
+    res.redirect(307, '/database/delete_tags');
+  }
 });
 
 
