847f343a782533dfeb7e5150959bdbc13db84b8e
CatPleb <48354679+CatPleb@users.noreply.github.com>
2019-09-26 12:52:29 +0200

    v0.3.2
    fixed Tag bug

--- routes/search.js
+++ routes/search.js
@@ -41,41 +41,33 @@ router.get('/', async function(req, res, next) {
     })
 
   } else if (selectedtag) {
-
-    /* Get ids from searchtag and all children */
-    Hierarchy.find({ name: { $in: selectedtag }}, async function(err, doc) {
-      
-      /* Now search for every exercise which has one of those ids in it */
-      var promises_tags = selectedtag.map(async function(stag) {
-        return Hierarchy.find({ name: stag });
-      });
-      
-      Promise.all(promises_tags).then((db_tags) => {
-        
-        newtagList = []
-        for (i=0;i<db_tags.length;i++) {
-          newtagList = newtagList.concat(db_tags[i][0].subtree_tags);
-        }
-
-        Exercises.find( { tags: { $in: newtagList }}, function(err,result) {
-
-          var public_result = [];
-          for (i=0; i<result.length; i++) {
-
-            public_result.push({
-              name: result[i].name,
-              png: result[i].png,
-              tags: result[i].tags,
-              public_id: ('/exercises/id/'+result[i].public_id),
-            });
-
-          }
-          res.render('search/search', {result: public_result, taglist: req.session.taglist, selectedtag: selectedtag});
-        });
-      })  
+    var promises_tags = selectedtag.map(async function(stag) {
+      return Hierarchy.find({ name: stag });
     });
+    
+    Promise.all(promises_tags).then((db_tags) => {
       
+      newtagList = []
+      for (i=0;i<db_tags.length;i++) {
+        newtagList = newtagList.concat(db_tags[i][0].subtree_tags);
+      }
 
+      Exercises.find( { tags: { $in: newtagList }}, function(err,result) {
+
+        var public_result = [];
+        for (i=0; i<result.length; i++) {
+
+          public_result.push({
+            name: result[i].name,
+            png: result[i].png,
+            tags: result[i].tags,
+            public_id: ('/exercises/id/'+result[i].public_id),
+          });
+
+        }
+        res.render('search/search', {result: public_result, taglist: req.session.taglist, selectedtag: selectedtag});
+      });
+    });     
   } else {
     res.render('search/search', {taglist: req.session.taglist});
   }
